a35021bcf49938ede207ef46af53b865aedc0317
Ravi Kumar <ravikumar@IN-FVFGJ275Q05P.local>
2022-03-31 10:33:22 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,87 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import {
+  BackHandler,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native';
+import { WebView } from 'react-native-webview';
+import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
+
+class App extends Component {
+  webView = {
+    canGoBack: true,
+    ref: null,
+  }
+  onAndroidBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  componentDidMount() {
+    // GetLocation.getCurrentPosition({
+    //   enableHighAccuracy: true,
+    //   timeout: 15000,
+    // })
+    //   .then(location => {
+    //     console.log('location:', location);
+    //   })
+    //   .catch(error => {
+    //     const { code, message } = error;
+    //     console.warn(code, message);
+    //   })
+    RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
+      interval: 10000,
+      fastInterval: 5000,
+    })
+      .then((data) => {
+        console.log('data:', data);
+        // The user has accepted to enable the location services
+        // data can be :
+        //  - "already-enabled" if the location services has been already enabled
+        //  - "enabled" if user has clicked on OK button in the popup
+      })
+      .catch((err) => {
+        console.log('error:', err);
+        // The user has not accepted to enable the location services or something went wrong during the process
+        // "err" : { "code" : "ERR00|ERR01|ERR02|ERR03", "message" : "message"}
+        // codes :
+        //  - ERR00 : The user has clicked on Cancel button in the popup
+        //  - ERR01 : If the Settings change are unavailable
+        //  - ERR02 : If the popup has failed to open
+        //  - ERR03 : Internal error
+      });
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress');
+    }
+  }
+
+  render() {
+    return (
+      <WebView source={{
+        uri: "https://codepen.io/ocinpp/pen/EpbXKz"
+      }}
+        ref={(webView) => { this.webView.ref = webView; }}
+        geolocationEnabled={true}
+        onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }} />
+    );
+  };
+}
+
+export default App;
